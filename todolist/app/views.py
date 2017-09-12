a0d6a0fcbdc3bdca11fa78b49af642bd00302adf
Tristan <tristanmartin2016@gmail.com>
2017-09-12 17:16:11 -0500

    Added todolist functionality and database configuration

--- todolist/app/views.py
+++ todolist/app/views.py
@@ -1,5 +1,22 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import Todo
 
 def index(request):
-    return HttpResponse("Hello world!")
+    todos = Todo.objects.all()[:10]
+    context = {
+        'todos':todos
+    }
+    return render(request, 'index.html', context)
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/')
+    else:
+        return render(request, 'add.html')
